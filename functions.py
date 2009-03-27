8b1dab4b613fa39d805251b2fec91d1c7a141d02
Chris Drackett <chris@chrisdrackett.com>
2009-03-26 22:15:04 -0500

    using elsewhere to find a users urls from socialgraph. This probably could use some optimization, but for now its working. Next steps are to create a form and actually populate this data for a user.

--- functions.py
+++ functions.py
@@ -1,12 +1,47 @@
+import re
 import urllib
 
 from django.utils import simplejson as json
+
+from elsewhere.models import SocialNetwork
 
 def get_connections(urls):
     lookup_url = 'http://socialgraph.apis.google.com/lookup?q=%s&fme=1&edo=1&edi=1' % ','.join(urls)
     info_from_google = json.loads(urllib.urlopen(lookup_url).read())
 
-    return info_from_google
+    nodes = info_from_google['nodes']
+
+    claimed_nodes = []
+    unverified_claiming_nodes = []
+    nodes_referenced = []
+    nodes_referenced_by = []
+
+    for node in nodes.values():
+        claimed_nodes = claimed_nodes + node['claimed_nodes']
+        unverified_claiming_nodes = unverified_claiming_nodes + node['unverified_claiming_nodes']
+
+        # for key in node['nodes_referenced'].keys():
+        #     if 'me' in node['nodes_referenced'][key]['types']:
+        #         nodes_referenced.append(key)
+        # 
+        # for key in node['nodes_referenced_by'].keys():
+        #     if 'me' in node['nodes_referenced_by'][key]['types']:
+        #         nodes_referenced.append(key)
+
+    all_nodes = claimed_nodes + unverified_claiming_nodes + nodes_referenced + nodes_referenced_by
+
+    all_networks = list(SocialNetwork.objects.all().only('url'))
+
+    final_list = []
+
+    for item in all_nodes:
+        for network in all_networks:
+            if re.match(network.url % '[\w-]+', item):
+                final_list.append(item)
+
+    return final_list
+
+
 
 # http://pownce.com/[\w-]+/something
 
